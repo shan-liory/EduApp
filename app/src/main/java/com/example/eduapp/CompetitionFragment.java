7f30ba17e610c5c27eb0f3a5d11bfec955963fcd
Pamela <pamela.k@imail.sunway.edu.my>
2021-11-09 04:40:39 +0800

    popfox

--- app/src/main/java/com/example/eduapp/CompetitionFragment.java
+++ app/src/main/java/com/example/eduapp/CompetitionFragment.java
@@ -4,16 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.example.eduapp.MainViewModel;
 import com.example.eduapp.R;
 
 public class CompetitionFragment extends BaseFragment {
+
+    private Button mPopcatButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,6 +28,14 @@ public class CompetitionFragment extends BaseFragment {
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        mPopcatButton = view.findViewById(R.id.popcatButton);
+        mPopcatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_navigation_notifications_to_popcatFragment);
+            }
+        });
 
     }
 
