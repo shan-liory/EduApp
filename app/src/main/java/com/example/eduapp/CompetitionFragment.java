1c43d5d65a6c4bdf8cba087381d51783541527f6
Pamela <pamela.k@imail.sunway.edu.my>
2021-11-11 13:09:54 +0800

    github game

--- app/src/main/java/com/example/eduapp/CompetitionFragment.java
+++ app/src/main/java/com/example/eduapp/CompetitionFragment.java
@@ -1,9 +1,11 @@
 package com.example.eduapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +14,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.eduapp.MainViewModel;
 import com.example.eduapp.R;
+import com.example.eduapp.ihavetofly.MinigameActivity;
 
 public class CompetitionFragment extends BaseFragment {
 
@@ -24,6 +27,17 @@ public class CompetitionFragment extends BaseFragment {
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Button button=(Button) view.findViewById(R.id.minigameButton);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(getActivity().getApplicationContext(), MinigameActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 
